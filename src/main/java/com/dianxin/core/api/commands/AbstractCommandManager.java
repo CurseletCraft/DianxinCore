a04df39caba0ceae6a1a2d93c8f2bc6413b3c894
hungdx-t1 <nguyenlevinhhungnt153@gmail.com>
2025-11-28 13:36:27 +0700

    added AbstractCommandManager

---
+++ src/main/java/com/dianxin/core/api/commands/AbstractCommandManager.java
@@ -0,0 +1,19 @@
+package com.dianxin.core.api.commands;
+
+import com.dianxin.core.api.JavaDiscordBot;
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.requests.restaction.CommandListUpdateAction;
+import org.jetbrains.annotations.NotNull;
+
+public abstract class AbstractCommandManager<T extends JavaDiscordBot> extends ListenerAdapter {
+    private final T bot;
+
+    public AbstractCommandManager(T bot) {
+        this.bot = bot;
+    }
+
+    public abstract void onSlashCommandInteraction(@NotNull SlashCommandInteractionEvent event);
+
+    public abstract void inits(CommandListUpdateAction commandsTree);
+}
